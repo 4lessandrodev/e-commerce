f15b6ea9797d3c33417bbfd7660989c311372cfa
Alessandro <alessandroadm@live.com>
2020-11-29 13:34:06 -0300

    feat: add validate email util function

---
+++ src/Domain/utils/index.ts
@@ -0,0 +1,6 @@
+export * from './convert-negative-number-to-positive.domain.util';
+export * from './convert-positive-number-to-negative.domain.util';
+export * from './format-number-to-currency.domain.util';
+export * from './transform-monetary-two-digits.domain.utils';
+export * from './validate-email.domain.util';
+export * from './transform-string-to-lowercase.domain.util';
---
+++ src/Domain/utils/validate-email.domain.util.ts
@@ -0,0 +1,4 @@
+import isEmail from 'validator/lib/isEmail';
+export const isValidEmail = (email: string): boolean => {
+  return isEmail(email);
+};
