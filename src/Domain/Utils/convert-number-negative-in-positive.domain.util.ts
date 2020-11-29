8f3c83f472b8e7d352f35ee7f8896956320f710e
Alessandro <alessandroadm@live.com>
2020-11-29 01:03:46 -0300

    refactor: rename files and update function name

--- src/Domain/Utils/convert-number-negative-in-positive.domain.util.ts
+++
@@ -1,6 +0,0 @@
-export const convertNegativeNumberToPositive = (value: number): number => {
-  if (value > 0) {
-    return value;
-  }
-  return value * -1;
-};
--- src/Domain/Utils/format-number-to-currency.domain.util.ts
+++
@@ -1,6 +0,0 @@
-export const FormatNumberToCurrency = (value: number): string =>
-  value.toLocaleString('pt-BR', {
-    style: 'currency',
-    minimumFractionDigits: 2,
-    currency: 'BRL',
-  });
