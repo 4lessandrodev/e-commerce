b2b52f22508403a6f32e2b80f896135873e503e0
Alessandro <alessandroadm@live.com>
2020-11-29 21:22:43 -0300

    test: add password value object

---
+++ src/Domain/value-objects/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email.value-object';
+export * from './image/image.value-object';
+export * from './monetary/monetary.value-object';
+export * from './password/password.value-object';
---
+++ src/Domain/value-objects/password/password.value-object.spec.ts
@@ -0,0 +1,77 @@
+import {
+  ERROR_INVALID_PASSWORD_MAX_LENGTH,
+  ERROR_INVALID_PASSWORD_MIN_LENGTH,
+} from './password-errors.domain';
+import { PasswordValueObject } from './password.value-object';
+
+describe('password.value-object', () => {
+  it('Should return a valid password ', () => {
+    const createdPassword = PasswordValueObject.create('valid_password');
+    expect(createdPassword.isFailure).toBe(false);
+    expect(createdPassword.getResult().getValue()).toBe('valid_password');
+    expect(createdPassword.getResult().isAlreadyEncrypted()).toBe(false);
+  });
+
+  it('Should return a invalid password min length message', () => {
+    const createdPassword = PasswordValueObject.create('invalid');
+    expect(createdPassword.isFailure).toBe(true);
+    expect(createdPassword.errorValue()).toBe(
+      ERROR_INVALID_PASSWORD_MIN_LENGTH,
+    );
+  });
+
+  it('Should return a invalid password max length message', () => {
+    const createdPassword = PasswordValueObject.create(
+      'invalid_message_max_length',
+    );
+    expect(createdPassword.isFailure).toBe(true);
+    expect(createdPassword.errorValue()).toBe(
+      ERROR_INVALID_PASSWORD_MAX_LENGTH,
+    );
+  });
+
+  it('Should return a invalid password max length message', async () => {
+    const createdPassword = PasswordValueObject.create('valid_password');
+    await createdPassword.getResult().encryptPassword();
+    const isEncripted = createdPassword.getResult().isAlreadyEncrypted();
+    expect(createdPassword.isFailure).toBe(false);
+    expect(isEncripted).toBe(true);
+  });
+
+  it('Should regenerate a password with encripted value', async () => {
+    const createdPassword = PasswordValueObject.create('valid_password');
+    await createdPassword.getResult().encryptPassword();
+    createdPassword.getResult().isAlreadyEncrypted();
+
+    const generateEncriptedValue = PasswordValueObject.create(
+      createdPassword.getResult().getValue(),
+    );
+
+    expect(generateEncriptedValue.isFailure).toBe(false);
+    expect(createdPassword.isFailure).toBe(false);
+    expect(generateEncriptedValue.getResult().getValue()).toBe(
+      createdPassword.getResult().getValue(),
+    );
+    expect(generateEncriptedValue.getResult().isAlreadyEncrypted()).toBe(true);
+  });
+
+  it('Shoud compare a not encrypted password and return true if match and false if not', async () => {
+    const password = PasswordValueObject.create('valid_password').getResult();
+    const matchPass = await password.comparePassword('valid_password');
+    const notMatchPass = await password.comparePassword('invalid_password');
+
+    expect(matchPass).toBe(true);
+    expect(notMatchPass).toBe(false);
+  });
+
+  it('Shoud compare an encrypted password and return true if match and false if not', async () => {
+    const password = PasswordValueObject.create('valid_password').getResult();
+    await password.encryptPassword();
+
+    const matchPass = await password.comparePassword('valid_password');
+    const notMatchPass = await password.comparePassword('invalid_password');
+
+    expect(matchPass).toBe(true);
+    expect(notMatchPass).toBe(false);
+  });
+});
