5b0bb496c56ef27a48e922bd752b159478ad2689
Alessandro <alessandroadm@live.com>
2020-12-10 00:08:33 -0300

    feat: add ProductCategory

---
+++ src/Domain/entities/product-category/ProductCategory.domain-entity.ts
@@ -0,0 +1,41 @@
+import {
+  BaseDomainEntity,
+  Entity,
+  Result,
+  UniqueEntityID,
+} from '../../../Shared';
+import { validateStringLengthBetweenMaxAndMin } from '../../utils';
+import { ERROR_PRODUCT_CATEGORY_DESCRIPTION_LENGTH } from './ProductCategoryErrors.domain-entity';
+export const PRODUCT_CATEGORY_DESCRIPTION_MAX_STRING_LENGTH = 20;
+export const PRODUCT_CATEGORY_DESCRIPTION_MIN_STRING_LENGTH = 3;
+
+interface ProductCategoryProps extends BaseDomainEntity {
+  description: string;
+}
+
+export class ProductCategory extends Entity<ProductCategoryProps> {
+  private constructor(props: ProductCategoryProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+
+  get description(): string {
+    return this.props.description;
+  }
+
+  public static create(
+    props: ProductCategoryProps,
+    id?: UniqueEntityID,
+  ): Result<ProductCategory> {
+    const isValidDescription = validateStringLengthBetweenMaxAndMin({
+      text: props.description,
+      minLength: PRODUCT_CATEGORY_DESCRIPTION_MIN_STRING_LENGTH,
+      maxLength: PRODUCT_CATEGORY_DESCRIPTION_MAX_STRING_LENGTH,
+    });
+    if (!isValidDescription) {
+      return Result.fail<ProductCategory>(
+        ERROR_PRODUCT_CATEGORY_DESCRIPTION_LENGTH,
+      );
+    }
+    return Result.ok<ProductCategory>(new ProductCategory(props, id));
+  }
+}
---
+++ src/Domain/entities/product-category/ProductCategoryId.domain-entity.ts
@@ -0,0 +1,14 @@
+import { Entity, UniqueEntityID } from '../../../Shared';
+
+export class ProductCategoryId extends Entity<any> {
+  get id(): UniqueEntityID {
+    return this._id;
+  }
+  private constructor(id?: UniqueEntityID) {
+    super(null, id);
+  }
+
+  public static create(id?: UniqueEntityID): ProductCategoryId {
+    return new ProductCategoryId(id);
+  }
+}
