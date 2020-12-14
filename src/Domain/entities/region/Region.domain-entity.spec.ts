dfb8215a368a48f9985d4ed7e605db517d8b6974
Alessandro <alessandroadm@live.com>
2020-12-14 00:16:34 -0300

    test: add region entity create region entity on domain #7

---
+++ src/Domain/entities/region/Region.domain-entity.spec.ts
@@ -0,0 +1,89 @@
+import { Result, UniqueEntityID } from '../../../Shared';
+import { MonetaryValueObject } from '../../value-objects';
+import { Region } from './Region.domain-entity';
+import { RegionProps } from './Region.domain-entity-interface';
+import { ERROR_REGION_DESCRIPTION_LENGTH } from './RegionErrors.domain-entity';
+import { RegionId } from './RegionId.domain-entity';
+
+describe('Region.domain-entity', () => {
+  const makeSut = (
+    props?: RegionProps,
+    id?: UniqueEntityID,
+  ): Result<Region> => {
+    return Region.create(
+      {
+        description: props?.description ?? 'Valid Description',
+        freigthPrice:
+          props?.freigthPrice ?? MonetaryValueObject.create(10).getResult(),
+        geoCode: props?.geoCode,
+        isActive: props?.isActive,
+      },
+      id,
+    );
+  };
+
+  it('Should create a valid region ', () => {
+    const regionCreated = makeSut();
+    expect(regionCreated.isFailure).toBe(false);
+    expect(regionCreated.getResult().description).toBe('Valid Description');
+    expect(regionCreated.getResult().id).not.toBeUndefined();
+    expect(regionCreated.getResult().id).not.toBeNull();
+    expect(regionCreated.getResult().isActive).toBe(true);
+    expect(regionCreated.getResult().freigthPrice.value).toBe(10);
+    expect(regionCreated.getResult().geoCode).toBe(0);
+    expect(regionCreated.getResult().isDeleted).toBe(false);
+  });
+
+  it('Should deactivate with success a region ', () => {
+    const regionCreated = makeSut().getResult();
+    expect(regionCreated.isActive).toBe(true);
+    expect(regionCreated.isDeleted).toBe(false);
+    regionCreated.deactivate();
+    expect(regionCreated.isActive).toBe(false);
+    expect(regionCreated.isDeleted).toBe(false);
+  });
+
+  it('Should active with success a region ', () => {
+    const regionCreated = makeSut({
+      description: 'Valid description',
+      freigthPrice: MonetaryValueObject.create(11).getResult(),
+      isActive: false,
+    }).getResult();
+    expect(regionCreated.isActive).toBe(false);
+    expect(regionCreated.isDeleted).toBe(false);
+    regionCreated.activate();
+    expect(regionCreated.isActive).toBe(true);
+    expect(regionCreated.isDeleted).toBe(false);
+  });
+
+  it('Should active with success a region ', () => {
+    const createdId = RegionId.create().id;
+    const regionCreated = makeSut(
+      {
+        description: 'Valid description',
+        freigthPrice: MonetaryValueObject.create(11).getResult(),
+      },
+      createdId,
+    ).getResult();
+    expect(regionCreated.id.toString()).toBe(createdId.toString());
+  });
+
+  it('Should fail if provide a short description ', () => {
+    const regionResult = makeSut({
+      description: 'a',
+      freigthPrice: MonetaryValueObject.create(15).getResult(),
+    });
+    expect(regionResult.isFailure).toBe(true);
+    expect(regionResult.error).toBe(ERROR_REGION_DESCRIPTION_LENGTH);
+  });
+
+  it('Should fail if provide a long description ', () => {
+    const regionResult = makeSut({
+      description:
+        'this_is_a_long_region_description_to_validate_max_length_description_error',
+      freigthPrice: MonetaryValueObject.create(15).getResult(),
+    });
+    expect(regionResult.isFailure).toBe(true);
+    expect(regionResult.error).toBe(ERROR_REGION_DESCRIPTION_LENGTH);
+  });
+});
