43c98f666d78ad68768490db6128c4baf855e53b
Alessandro <alessandroadm@live.com>
2020-12-28 00:04:24 -0300

    test: add method remove product from basket

--- src/Domain/entities/item/ItemBasket.domain-entity.spec.ts
+++ src/Domain/entities/item/ItemBasket.domain-entity.spec.ts
@@ -5,7 +5,7 @@ import { ERROR_ITEM_INVALID_QUANTITY } from './ItemErrors.domain-entity';
 import { ItemBasket } from './ItemBasket.domain-entity';
 import { Basket, Product } from '../../aggregates-root';
 import { BasketCategory } from '../basket-category/BasketCategory.domain-entity';
-import { image, random } from 'faker';
+import { image } from 'faker';
 import { ProductCategory } from '../product-category/ProductCategory.domain-entity';
 import { ItemId } from './ItemId.domain-entity';
 
@@ -21,9 +21,9 @@ describe('ItemBasket.domain-entity', () => {
           Basket.create({
             category: BasketCategory.create({
               changesLimit: 3,
-              description: random.words(2),
+              description: 'Valid Description',
             }).getResult(),
-            description: random.words(2),
+            description: 'Valid Description',
             images: [
               ImageValueObject.create(image.imageUrl().toString()).getResult(),
             ],
@@ -32,9 +32,9 @@ describe('ItemBasket.domain-entity', () => {
             products: [
               Product.create({
                 category: ProductCategory.create({
-                  description: random.words(1),
+                  description: 'Valid Category',
                 }).getResult(),
-                description: random.word(),
+                description: 'Valid Description',
                 images: [
                   ImageValueObject.create(
                     image.imageUrl().toString(),
@@ -66,6 +66,7 @@ describe('ItemBasket.domain-entity', () => {
 
   it('Should fail if provide a negative number', () => {
     const props = makeSut().getResult().props;
+
     const itemCreated = makeSut({
       ...props,
       quantity: -10,
